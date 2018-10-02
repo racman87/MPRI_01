0d7d28b7fef17377449bc4e6fbffe6572b3cc3d8
louis <louis@debian.debian>
2018-10-02 17:15:09 +0200

    part 1 ok

--- bike_prediction.py
+++ bike_prediction.py
@@ -4,7 +4,9 @@ import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
 """
     This method loads the data for a specific station id, processes it and the train and evaluate
@@ -22,6 +24,8 @@ def evaluate(station_id, plot_gini_coef=False, use_time_features=False, show_plo
 
     ## Load data
     #TODO CP1 load the data
+    dfStation0 = load_data(station_id)
+
 
     ## Process data (Note: data could also be normalized and/or one-hot encoded for better results)
     if use_time_features:
@@ -30,42 +34,74 @@ def evaluate(station_id, plot_gini_coef=False, use_time_features=False, show_plo
 
     ## Split the data into train and test sets (important keep the temporal order)
     #TODO CP1 split the dataframe into train and test sets (75% - 25%)
+    train_set,test_set = train_test_split(dfStation0, test_size=0.25,shuffle=False)
 
     ## Split the train_set into X_train (input data), y_train (output data)
     #TODO CP1 split the train set to isolate input from output data
+    y_train = train_set.loc[:,'bikes(t+15)':'bikes(t+60)']
+    X_train = train_set.loc[:,'bikes(t-20)':'bikes(t)']
+
 
     ## Split the test_set into X_test (input data), y_test (output data, aka ground truth)
     # TODO CP1 split the test set to isolate input from output data
+    y_test = test_set.loc[:,'bikes(t+15)':'bikes(t+60)']
+    X_test = test_set.loc[:,'bikes(t-20)':'bikes(t)']
+
+    #print(y_test.iloc[1:50])
 
     ## Initialize the ML model
     #TODO CP1 initialize your RandomForestRegressor
+    classifier = RandomForestRegressor()
 
     ## Train the model using the X_test and y_test data
     #TODO CP1 Train your model (model.fit() function)
+    classifier.fit(X_train,y_train)
 
     ## Perform the predictions for the X_test set
     #TODO CP1 Perform (and get) the predictions with the trained model
+    y_hat_test = classifier.predict(X_test)
 
     ## Convert the received predictions (list of lists) to a dataframe (specify columns names)
     #TODO CP1 Convert the predictions back to a pandadataframe (you may use other solutions here)
+    y_hat_test = pandas.DataFrame(y_hat_test, columns=['bikes(t+15)','bikes(t+30)','bikes(t+60)'])
+    y_hat_test = y_hat_test.round(0).astype(int)
+
+    #print(y_hat_test.iloc[1:50])
 
     ## Compare the obtained predictions and the ground_truth(y_test_set)
     #TODO CP1 Compare the predictions and the groundtruth and measure the Mean Aboslute Error (of each horizon)
-    mean_absolute_error = [0, 0, 0] #TODO CP1 - You can directly compute the mean on the error dataframe
+
+
+    y_hat_test = y_hat_test.reset_index(drop=True)
+    y_test = y_test.reset_index(drop=True)
+
+    # [0,0,0]TODO CP1 - You can directly compute the mean on the error dataframe
+    mean_absolute_error = metrics.mean_absolute_error(y_test,y_hat_test,multioutput='raw_values')
 
     print("Mean absolute error: {}".format(mean_absolute_error))
+
+    print("Estimator :",classifier.n_estimators)
 
     ## Plot Mean Absolute Error
     if show_plot:
         #TODO CP1 Generate the bar plot the plot for the mean absolute error per horizon
+
+        plt.figure(1)
+        plt.bar(["t+15","t+30","t+60"],mean_absolute_error)
         plt.title('Mean absolute error (Station <{}>)'.format(station_id))
         plt.tight_layout()
-        plt.show()
 
     ##Plot Gini coefficients and their importance
     if show_plot:
         # TODO CP1 Generate the bar plot for the feature importance from the model (Gini coefficients)
-        plt.tight_layout()
+
+        giniCoef = classifier.feature_importances_
+
+        plt.figure(2)
+        plt.bar(X_test.columns.values,giniCoef)
+        plt.subplots_adjust(bottom=0.5)
+        #plt.tight_layout()
+        plt.xticks(rotation='vertical')
         plt.title('Gini coefficients (Station <{}>)'.format(station_id))
         plt.show()
 
